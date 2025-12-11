2cbef3d91bef6b4768687ad63cc3556c7a55e877
Addy Williams <awillia5@gmail.com>
2025-12-10 19:06:55 -0800

    fedhkve

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
+import Navbar from "./components/Navbar";
+import ProjectDetailsPage from "./pages/ProjectDetailsPage";
+import AuthPage from "./pages/AuthPage";
+
+
+function App() {
+  return (
+    <>
+      <div className="p-5 bg-zinc-900 h-screen">
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/projects" element={<ProjectsPage />}/>
+          <Route path="/projects/:projectId" element={<ProjectDetailsPage />}/>
+          <Route path="/auth" element={<AuthPage />}/>
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-white">Welcome!</h1>
+    </div>
+  );
+}
+
+export default HomePage;
