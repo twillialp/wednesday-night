32d8c916ed2a5c47377e4775fd9abc9cd0262153
Addy Williams <awillia5@gmail.com>
2025-12-11 06:40:37 -0800

    add image

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ function HomePage() {
       <h1 className="text-4xl text-center mt-8 font-bold text-white">Welcome!</h1>
     </div>
     <div>
-    <img className="w-full h-auto" src="https://unsplash.com/photos/brown-pencil-on-white-printing-paper-fteR0e2BzKo" alt="Foreground image of a paper and pencil project"></img>
+ <img src="/sven-mieke.jpg" alt="Icon" />
     </div>
 </>
   );
