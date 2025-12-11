e414199e8c8c879b6bae14843afd7bc390520724
Addy Williams <awillia5@gmail.com>
2025-12-11 08:42:46 -0800

    Updated app name

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ function HomePage() {
   return (
     <>
     <div >
-      <h1 className="text-4xl text-center mt-8 font-bold text-white">Welcome!</h1>
+      <h1 className="text-4xl text-center mt-8 font-bold text-white">Welcome to Discombobulated!</h1>
     </div>
     <div>
  <img src="/sven-mieke.jpg" alt="Icon" />
